23ef566fda63dbd7250294f6d8bad834d634defd
hilmi997 <hilmihidayat597@gmail.com>
2019-11-21 15:18:02 +0700

    hello

---
+++ app/src/main/java/phonebase/hilmi/kartar/services/AppServices.java
@@ -0,0 +1,10 @@
+package phonebase.hilmi.kartar.services;
+
+/**
+ * Created by User on 21/11/2019.
+ */
+
+public class AppServices {
+    public String IP_LOKAL = "http://";
+    public String IP_PUBLIK = "";
+}
